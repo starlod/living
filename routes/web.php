bafff157bbc8104350d4e8bded581ca38df587de
yuki <starlod.ggl@gmail.com>
2017-05-14 19:48:48 +0900

    php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'MoviesController@index')->name('home');
+
+Route::get('/movies', 'MoviesController@index')->name('movies.index');
+Route::get('/movies/create', 'MoviesController@create')->name('movies.create');
+Route::get('/movies/{id}', 'MoviesController@show')->name('movies.show');
+Route::post('/movies', 'MoviesController@store')->name('movies.store');
+Route::get('/movies/{id}/edit', 'MoviesController@edit')->name('movies.edit');
+Route::put('/movies/{id}', 'MoviesController@update')->name('movies.update');
+Route::delete('/movies/{id}', 'MoviesController@destroy')->name('movies.destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
