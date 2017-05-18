82a9c8b32bb66cb8a334c1da5103f07ca84e4efd
yuki <starlod.ggl@gmail.com>
2017-05-18 23:42:10 +0900

    ユーザークラスに存在確認、追加処理を追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,32 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * ユーザー存在確認
+     *
+     * @param  array  $data
+     * @return User
+     */
+    public static function isExists(array $data)
+    {
+        return self::where('name', $data['name'])
+            ->orWhere('email', $data['email'])
+            ->exists();
+    }
+
+    /**
+     * ユーザー新規作成
+     *
+     * @param  array  $data
+     * @return User
+     */
+    public static function add(array $data)
+    {
+        return self::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
 }
